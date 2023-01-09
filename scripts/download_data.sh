b70b9a82e785cda7218b5f1b68f4a8bcb0aceedc
Aleksander Hołyński alik.holynski@gmail.com <alik.holynski@gmail.com>
2023-01-09 14:28:16 +0000

    Unzip and delete zip files in download script

--- scripts/download_data.sh
+++ scripts/download_data.sh
@@ -7,5 +7,9 @@ mkdir -p $SCRIPT_DIR/../data
 wget http://instruct-pix2pix.eecs.berkeley.edu/gpt-generated-prompts.jsonl -O $SCRIPT_DIR/../data/gpt-generated-prompts.jsonl
 wget http://instruct-pix2pix.eecs.berkeley.edu/human-written-prompts.jsonl -O $SCRIPT_DIR/../data/human-written-prompts.jsonl
 
+if [ -z $1 ] then exit 0 fi
+
 mkdir $SCRIPT_DIR/../data/$1
 wget -A zip,json -r http://instruct-pix2pix.eecs.berkeley.edu/$1 -nd -P $SCRIPT_DIR/../data/$1
+unzip $SCRIPT_DIR/../data/$1/*.zip -d $SCRIPT_DIR/../data/$1/
+rm -f $SCRIPT_DIR/../data/$1/*.zip
